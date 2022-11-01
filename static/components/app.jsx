19545fa4633acf0e1eaeb7a0b47dc378a6335bda
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-11-01 15:36:52 -0500

    Successfully display restaurant card

---
+++ static/components/app.jsx
@@ -0,0 +1,12 @@
+// import GreetJared from "./greetJared.jsx";
+
+function App() {
+    return (
+        <div>
+            <Card />
+        </div>
+    );
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
