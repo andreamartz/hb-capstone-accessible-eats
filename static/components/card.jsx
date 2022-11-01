85563c7499053660d2053cfc7212339e6cd4528a
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-11-01 15:35:31 -0500

    Build single card component with hard-coded business id

--- static/components/card.jsx
+++ static/components/card.jsx
@@ -1,16 +1,40 @@
-function Card() {
+"use strict";
+
+function Card(props) {
+    const [businessDetails, setBusinessDetails] = React.useState({});
+    const [coordinates, setCoordinates] = React.useState({});
+    React.useEffect(() => {
+        const getBusinessDetails = (businessId) => {
+            const BASE_URL = 'http://localhost:5000/businesses'
+
+            const url = `${BASE_URL}/${businessId}`;
+        
+            fetch(url)
+                .then((response) => response.json())
+                .then((jsonData) => {
+                    setBusinessDetails(jsonData);
+                    setCoordinates(jsonData.coordinates)
+                    console.log(jsonData.coordinates);
+                });
+        }
+        // TODO: replace hard-coded business id with passed in id from props
+        getBusinessDetails("-JxgWP3A3n8cIfDpwZQ90w");
+    }, []);
+    
     return (
         <div className="card mb-3">
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src="/static/img/food-pic.jpg" className="img-fluid rounded-start" alt="" />
+                    {/* <img src="https://s3-media1.fl.yelpcdn.com/bphoto/9ejXGr29i_Qon28S9uQcHg/o.jpg" className="img-fluid rounded-start" alt="" /> */}
+                    <img src={businessDetails.photo} className="img-fluid rounded-start" alt="" />
+
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
+                        <h5 className="card-title">{businessDetails.place_name}</h5>
+                        <p className="card-text">{businessDetails.display_address}</p>
+                        <p className="card-text">{businessDetails.display_phone}</p>
+                    </div>
                 </div>
             </div>
         </div>
