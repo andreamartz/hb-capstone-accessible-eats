6b5189cef05a9d2d0ae0a009f90ac88b287e60e6
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-11-01 15:11:43 -0500

    Add route for getting business details from Yelp API

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import (Flask, render_template, request, flash, session);
+from flask import (Flask, jsonify, render_template, request, flash, session)
 from model import connect_to_db, db
 from jinja2 import StrictUndefined
+import os
+import requests
+import json
 
 
 app = Flask(__name__)
@@ -24,6 +27,43 @@ def index():
     """Display homepage."""
 
     return render_template('index.html')
+
+
+# get business details
+@app.route('/businesses/<id>')
+def fcn(id):
+    """Get details about a business."""
+
+    payload = {"locale": "en_US" }
+    headers = {"Authorization": f"Bearer {YELP_FUSION_API_KEY}"}
+
+    # TODO: remove comment
+    # sample business id "-JxgWP3A3n8cIfDpwZQ90w"
+    url = f"{BASE_URL}/{id}"
+
+    res = requests.get(url, params=payload, headers=headers)
+    data = res.json()
+    print(data)
+
+    business_details = {
+        "place_name": data.get('name', None),
+        "coordinates": data.get('coordinates', None),
+        "display_phone": data.get('display_phone', None),
+        "display_address": data.get('display_address', None),
+    }
+    if data.get('photos'):
+        business_details["photo"] = data["photos"][0]
+
+    # for key in business_details:
+
+    #     if not data.get(key):
+    #         business_details[key] = "Unknown"
+
+    if not data.get('display_address'):
+        business_details["display_address"] = "Unknown"
+    
+    
+    return jsonify(business_details)
 
 
 if __name__ == "__main__":
