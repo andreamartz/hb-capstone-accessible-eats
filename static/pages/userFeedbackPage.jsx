785c1353b4ccf726e057a0917895cc4a02cc1495
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-11-09 16:45:56 -0600

    Implement user feedback page with CardList component

--- static/pages/userFeedbackPage.jsx
+++ static/pages/userFeedbackPage.jsx
@@ -1,7 +1,44 @@
 "use strict";
 
-const UserFeedbackPage = () => {
+const UserFeedbackPage = ({currentUser, 
+    pagesToShow,
+    setPagesToShow,
+    currentBusiness,
+    setCurrentBusiness,
+    businesses,
+    setBusinesses
+}) => {
+    console.log("CURRENT USER FROM USER FEEDBACK: ", currentUser);
+    React.useEffect(() => {
+        async function getUserFeedbacksOnMount() {
+            // const {id} = currentUser;
+            const id = currentUser?.id;
+            const result = await Api.getUserFeedbacks({id});
+            console.log("RESULT FEEDBACKS: ", result);
+
+            if (result) {
+                setBusinesses(result);
+            }
+        }
+        getUserFeedbacksOnMount();
+    }, [currentUser]);
+
+    if (!currentUser) {
+        return <p>You must be logged in to see this page!</p>
+    }
+    if (!businesses) {
+        return <h1>Loading...</h1>
+    }
     return (
-        <div>UserFeedbackPage</div>
+        <>
+            <div>UserFeedbackPage</div>
+
+            <CardList businesses={businesses}
+                pagesToShow={pagesToShow}
+                setPagesToShow={setPagesToShow}
+                currentBusiness={currentBusiness}
+                setCurrentBusiness={setCurrentBusiness}
+            />
+        </>
     )
 }
