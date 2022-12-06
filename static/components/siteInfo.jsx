c150eedae0195c2aa82a9808f692b59734b45331
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-12-06 16:26:19 -0600

    Add SiteInfo component

---
+++ static/components/siteInfo.jsx
@@ -0,0 +1,21 @@
+"use strict";
+
+const SiteInfo = ({
+}) => {
+
+    return (
+        <>
+        <div className="px-4 py5 my-5 text-center">
+            <h1 className="display-5 fw-bold">Welcome to Accessible Eats!</h1>
+            <div className="col-lg-8 mx-auto">
+                <p className="lead mb-4">
+                    Do you want to find the most accessible restaurants in your
+                    area for wheelchair users? Do you want to tell others 
+                    about the good and bad accessibility experiences you have 
+                    had when dining out? You're in the right place!
+                </p>
+            </div>
+        </div>
+        </>
+    );
+}
