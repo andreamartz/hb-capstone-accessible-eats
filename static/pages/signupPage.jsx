5a6c06ca425ec38b9c59a9d0e82ade79d63f5a4d
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2023-02-13 14:24:36 -0600

    Add explanatory comments to signupPage component

--- static/pages/signupPage.jsx
+++ static/pages/signupPage.jsx
@@ -1,5 +1,17 @@
 "use strict";
 
+/* This is the parent component of the SignupForm.
+
+   Business logic is handled here and the state-changing functions of handleChange, handleSubmit, and handleClick are passed to the presentational SignupForm child component.
+
+  The reducer function is located in the static/js/utils.js file. It contains logic for handling three user actions: 
+      * signup (i.e., user form submission), 
+      * getFeedback (for state changes related to failure or success of the submission, such as a success or error message for the user), and 
+      * updateField, which handles form control as the user types. 
+
+  The handleSubmit function calls the asynchronous signup method on the Api class (located at static/js/api.js), which sends an HTTP POST request to the backend to create a new user. 
+*/
+
 const SignupPage = ({pagesToShow, setPagesToShow, currentUser, setCurrentUser, }) => {
 
     const initialState = {
@@ -11,9 +23,10 @@ const SignupPage = ({pagesToShow, setPagesToShow, currentUser, setCurrentUser, }
         success: null,
         loading: false,
     }
-        
+
     const [ state, dispatch ] = React.useReducer(signupReducer, initialState);
 
+    // Handle form control as the user types
     const handleChange = async (evt) => {
         const { name, value } = evt.target;
         dispatch({ type: 'updateField', 
@@ -21,6 +34,7 @@ const SignupPage = ({pagesToShow, setPagesToShow, currentUser, setCurrentUser, }
         });
     }
 
+    // Handle user form submission
     const handleSubmit = async (evt) => {
         evt.preventDefault();
 
@@ -38,7 +52,9 @@ const SignupPage = ({pagesToShow, setPagesToShow, currentUser, setCurrentUser, }
 
         const result = await Api.signup(userData);
 
+        // SIGNUP WAS SUCCESSFUL; a new user account was created.
         if (result.success) {
+          // Note: Currently, the user state is handled at the top-level of the app. I will be refactoring this soon.
           setCurrentUser(user);
 
           dispatch({ type: 'getFeedback', payload: {
@@ -46,6 +62,8 @@ const SignupPage = ({pagesToShow, setPagesToShow, currentUser, setCurrentUser, }
             message: result.message,
             success: result.success,
           }});
+
+        // SIGNUP FAILED. The result.message tells us why.
         } else {
           dispatch({ type: 'getFeedback', payload: {
             loading: false,
